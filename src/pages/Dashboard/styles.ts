9c5e8d0680ac89bb098efd18096518c8e56c5a97
Djamilson <djamilson@gmail.com>
2020-09-04 16:45:03 -0300

    Add birdthDate

--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   margin: 0 3rem;
+  margin-top: 1rem;
 `;
 
 export const ProductList = styled.ul`
--- src/pages/Payment/styles.ts
+++ src/pages/Payment/styles.ts
@@ -3,6 +3,8 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   margin: 0 3rem;
+
+  margin-top: 1rem;
 `;
 
 export const Content = styled.div`
